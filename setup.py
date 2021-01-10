4952089558ceae20d24026e96b58e579783cc5b1
Zhu Mei <meizhu812@outlook.com>
2021-01-10 17:20:42 +0800

    [AC-1] created basic project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='affe_core',
+    version='0.0.1',
+    packages=['affe_core'],
+    url='https://github.com/ABL-TDM-Lab/AFFE-CORE',
+    license='GPLv3',
+    author='Zhu Mei',
+    author_email='meizhu812@outlook.com',
+    description='Core modules for data processing in PKU-AFFE program'
+)
