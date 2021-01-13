bdc3b28f9711b3945927690011e9bb971879fc66
Zhu Mei <meizhu812@outlook.com>
2021-01-14 00:29:32 +0800

    [AC-36] refine types

--- affe_core/util/finder.py
+++ affe_core/util/finder.py
@@ -4,19 +4,20 @@ Provide Finder classes to help find files of certain name or pattern in target d
 """
 import os
 from abc import ABC, abstractmethod
+from os import PathLike
 from typing import List, Optional, final
 
 
 class Finder(ABC):
     @final
-    def find_first_in(self, target_dir: str) -> Optional[str]:
+    def find_first_in(self, target_dir: PathLike) -> Optional[str]:
         for (cur_dir, dirs, files) in os.walk(target_dir):
             for file in files:
                 if self._match(file):
                     return os.path.abspath(os.path.join(cur_dir, file))
 
     @final
-    def find_all_in(self, target_dir: str) -> List[str]:
+    def find_all_in(self, target_dir: PathLike) -> List[str]:
         paths = []
         for (dir_name, dirs_here, files_here) in os.walk(target_dir):
             for file in files_here:
@@ -30,11 +31,11 @@ class Finder(ABC):
 
 
 class PatternFinder(Finder):
-    def __init__(self, *, init: str = "", ext=""):
+    def __init__(self, *, init: str = "", ext: str = ""):
         self._init = init
         self._ext = ext
 
-    def _match(self, file) -> bool:
+    def _match(self, file: str) -> bool:
         return file.startswith(self._init) and file.endswith(self._ext)
 
 
@@ -42,5 +43,5 @@ class FileNameFinder(Finder):
     def __init__(self, file_name: str):
         self._file_name = file_name
 
-    def _match(self, file) -> bool:
+    def _match(self, file: str) -> bool:
         return file is self._file_name
